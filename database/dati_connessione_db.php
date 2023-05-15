04db261aa6c9f5c979067b586db0333c33f19695
Riccardo Canteri <riccardo.canteri@liceobanfi.eu>
2023-05-15 13:08:06 +0200

    controllo dati db + db

---
+++ database/dati_connessione_db.php
@@ -0,0 +1,6 @@
+<?php 
+    $db_servername = "localhost";
+	$db_name = "biblioteca";
+	$db_username = "root";
+	$db_password = "";
+?>
---
+++ file/login.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+	require("../database/dati_connessione_db.php");
+	if (isset($_POST["username"])) {$username = $_POST["username"];} else {$username = "";}
+	if (isset($_POST["password"])) {$password = $_POST["password"];} else {$password = "";}
+	if (isset($_POST["tipologia"])) {$tipologia = $_POST["tipologia"];} else {$tipologia = "";}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Cwtch</title>
+    <link rel="stylesheet" href="styleCwach.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+    <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="../immagini/loghi/sfondo.png">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div>ciao</div>
+</body>
+
+</html>
